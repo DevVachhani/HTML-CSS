6b52a2e853f6d683391400c8419305e554bc4b08
Dev <devvachhani30@gmai.com>
2023-06-27 10:03:27 +0530

    updated : imdex.html

--- Astro motion/script.js
+++ Astro motion/script.js
@@ -1,1 +1,12 @@
+function barIcon() {
+  console.log("hello");
+  var x = document.getElementById("myLinks");
+  var y = document.getElementById("nav-mo");
 
+  if ((x.style.display = "none")) {
+    x.style.display = "block";
+    y.style.display = "none";
+  } else {
+    y.style.display = "none";
+  }
+}
